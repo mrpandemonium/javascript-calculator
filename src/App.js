c9de5f32a74b8c1e0b403d59146d55b3a80471cd
mrpandemonium <wincentsouriya@gmail.com>
2023-05-21 18:18:19 -0700

    added styles and event functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [display, setDisplay] = useState("0");
+
+  const handleNumber = (e) => {
+    const number = e.target.textContent;
+    if (display === "0") {
+      setDisplay(number);
+    } else {
+      setDisplay(display + number);
+    }
+  };
+
+  const handleOperator = (e) => {
+    const operator = e.target.textContent;
+    setDisplay(display + " " + operator + " ");
+  };
+
+  const handleEqual = () => {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div id="display" className="row">
+          {display}
+        </div>
+        <div id="clear" className="row">
+          AC
+        </div>
+        <div id="seven" onClick={handleNumber}>
+          7
+        </div>
+        <div id="eight" onClick={handleNumber}>
+          8
+        </div>
+        <div id="nine" onClick={handleNumber}>
+          9
+        </div>
+        <div id="multiply" onClick={handleOperator}>
+          *
+        </div>
+        <div id="four" onClick={handleNumber}>
+          4
+        </div>
+        <div id="five" onClick={handleNumber}>
+          5
+        </div>
+        <div id="six" onClick={handleNumber}>
+          6
+        </div>
+        <div id="divide" onClick={handleOperator}>
+          /
+        </div>
+        <div id="one" onClick={handleNumber}>
+          1
+        </div>
+        <div id="two" onClick={handleNumber}>
+          2
+        </div>
+        <div id="three" onClick={handleNumber}>
+          3
+        </div>
+        <div id="add" onClick={handleOperator}>
+          +
+        </div>
+        <div id="zero" onClick={handleNumber}>
+          0
+        </div>
+        <div id="decimal">.</div>
+        <div id="equals" onClick={handleEqual}>
+          =
+        </div>
+        <div id="subtract" onClick={handleOperator}>
+          -
+        </div>
+      </div>
     </div>
   );
 }
